cfdbc8c8032e23489d00e09e89428f1d5da22dcc
Ben Charrow <bcharrow@seas.upenn.edu>
2013-03-27 15:26:41 -0400

    Update calibrate.py

--- roboclaw/scripts/calibrate.py
+++ roboclaw/scripts/calibrate.py
@@ -7,6 +7,7 @@ import math
 
 import roslib; roslib.load_manifest('roboclaw')
 import rospy
+import tf.transformations
 
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
@@ -27,12 +28,19 @@ class Trial(object):
     def _command(self, twist, duration, distance, angle):
         # Record position
         odom_start = self.odom_pose
+        print odom_start
 
         # Move
         self.motor_pub.publish(twist)
-        rospy.sleep(duration)
+        if distance == 0:
+            time.sleep(1.0)
+            while abs(get_yaw(odom_start) - get_yaw(self.odom_pose)) > 0.05:
+                time.sleep(0.01)
+        else:
+            while (l2(odom_start, self.odom_pose) < distance - 0.02):
+                time.sleep(0.01)
 
-        # Stop 
+        # Stop
         t = Twist()
         t.linear.x = 0
         t.angular.z = 0
@@ -43,14 +51,15 @@ class Trial(object):
 
         # Record final location
         odom_end = self.odom_pose
+        print odom_end
         self.data.append((odom_start, odom_end, distance, angle))
-            
+
     def drive(self, distance):
         # distance is in meters
         print "Starting a translation trial...",
         sys.stdout.flush()
         t = Twist()
-        t.linear.x = 0.25
+        t.linear.x = 0.2
         duration  = distance / math.fabs(t.linear.x)
 
         self._command(t, duration, distance, 0)
@@ -61,12 +70,12 @@ class Trial(object):
         print "Starting a rotation trial...",
         sys.stdout.flush()
         t = Twist()
-        t.angular.z = -0.75
+        t.angular.z = -0.6
         duration  = angle / math.fabs(t.angular.z)
 
         self._command(t, duration, 0, angle)
         print "done"
-    
+
 
     def handle_odom_pose(self, msg):
         self.odom_pose = msg.pose.pose
@@ -76,14 +85,8 @@ def l2(pose1, pose2):
                      (pose1.position.y - pose2.position.y)**2)
 
 def get_yaw(pose):
-    quat = pose.orientation
-    mag = math.sqrt(quat.w**2 + quat.x**2 + quat.y**2 + quat.z**2)
-    quat.x /= mag
-    quat.y /= mag
-    quat.z /= mag
-    quat.w /= mag
-    
-    return math.atan(2*(quat.x*quat.w + quat.y*quat.z)/(1-2*(quat.z*quat.z + quat.w*quat.w)))
+    quat = [pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w]
+    return tf.transformations.euler_from_quaternion(quat)[2]
 
 def angle_err(pose1, pose2):
     return math.fabs(getYaw(pose1.orientation) - getYaw(pose2.orientation))
@@ -92,13 +95,13 @@ def pose_string(pose, fmt = "0.2f"):
     debug = "(x, y, theta): ({pos.x:{fmt:s}}, {pos.y:{fmt:s}}, {yaw:{fmt:s}})"
     return  debug.format(pos = pose.position, yaw = get_yaw(pose), fmt=fmt)
 
-def run(name, trials = 1, distance = 0.5):
+def run(name, trials = 1, distance = 0.75):
     rospy.init_node('calibrate')
     t = Trial(name)
     for i in range(trials):
         if not rospy.is_shutdown():
-            rospy.sleep(0.5)
-            # t.drive(distance = distance)
+            # t.drive(distance = 1.0)
+            # rospy.sleep(2.0)
             t.turn(math.pi * 2)
 
     odom_err_ttl = 0
@@ -106,15 +109,18 @@ def run(name, trials = 1, distance = 0.5):
         # print "Odom Start " + pose_string(odom_start)
         # print "Odom End   " + pose_string(odom_end)
 
+        print odom_start, odom_end
         odom_distance = l2(odom_start, odom_end)
 
         odom_err = math.fabs(odom_distance - distance)
         odom_err_ttl += odom_err
         print "Commanded Dist: %0.2f Odom Distance: %0.2f Error: %0.2f" % (
             distance, odom_distance, odom_err)
+        print "Start angle: %f End Angle: %f Diff: %f" % (
+            get_yaw(odom_start), get_yaw(odom_end), get_yaw(odom_end) - get_yaw(odom_start))
 
     print "Odometry Dist. Total Error:  %0.2f" % odom_err_ttl
-    
+
 if __name__ == "__main__":
     name = rospy.resolve_name(os.getenv('ROS_HOSTNAME'))
     run(name)
