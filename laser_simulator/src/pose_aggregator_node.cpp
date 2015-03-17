4cca38d4a89c395c3200bfd7280bea4855e44505
Philip Dames <pdames@seas.upenn.edu>
2015-03-17 13:34:23 -0400

    Update pose_aggregator to search for new pose topics

--- laser_simulator/src/pose_aggregator_node.cpp
+++ laser_simulator/src/pose_aggregator_node.cpp
@@ -1,11 +1,11 @@
+#include <map>
 #include <string>
 #include <vector>
 
 #include "ros/ros.h"
 
-#include "geometry_msgs/PoseStamped.h"
-#include "laser_simulator/PoseStampedNamed.h"
-#include "laser_simulator/PoseStampedNamedArray.h"
+#include <geometry_msgs/PoseStamped.h>
+#include <laser_simulator/PoseStampedNamedArray.h>
 
 #include <boost/thread/mutex.hpp>
 
@@ -13,83 +13,122 @@ using namespace std;
 
 
 class PoseSubscriber {
-  private:
-    ros::Subscriber sub_;
+private:
+  ros::Subscriber sub_;
 
-    boost::mutex pose_mutex_;
+  boost::mutex pose_mutex_;
 
-    string name_;
-    string frame_;
-    laser_simulator::PoseStampedNamed pose_;
+  string name_;
+  string frame_;
+  laser_simulator::PoseStampedNamed pose_;
 
-  public:
-    PoseSubscriber(ros::NodeHandle *node, string name,
-                   string frame, string pose_topic) :
-                   name_(name), frame_(frame) {
-      sub_ = node->subscribe(string("/") + name + string("/") + pose_topic,
-                      5, &PoseSubscriber::PoseCallback, this);
-    }
+  bool active_;
 
-    void PoseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
-      boost::mutex::scoped_lock lock(pose_mutex_);
-      pose_.header = msg->header;
-      pose_.pose = msg->pose;
-      pose_.child_frame_id = string("/") + name_ + string("/") + frame_;
-    }
+public:
+  PoseSubscriber(ros::NodeHandle *node, string name,
+                 string frame, string pose_topic) :
+                 name_(name), frame_(frame), active_(false) {
+    sub_ = node->subscribe(string("/") + name + string("/") + pose_topic,
+                    5, &PoseSubscriber::poseCallback, this);
+  }
 
-    laser_simulator::PoseStampedNamed GetPose() {
-      boost::mutex::scoped_lock lock(pose_mutex_);
-      return pose_;
-    }
+  void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
+    boost::mutex::scoped_lock lock(pose_mutex_);
+    active_ = true;
+
+    pose_.header = msg->header;
+    pose_.pose = msg->pose;
+    pose_.child_frame_id = string("/") + name_ + string("/") + frame_;
+  }
+
+  bool getPose(laser_simulator::PoseStampedNamed* pose) {
+    boost::mutex::scoped_lock lock(pose_mutex_);
+    *pose = pose_;
+    return sub_.getNumPublishers() > 0;
+  }
+
+  string getName(void) { return name_; }
+
+  int getNumPublishers(void) { return sub_.getNumPublishers(); }
+
+  bool active(void) { return active_; }
 };
 
 
 class PoseAggregator {
-  private:
-    ros::NodeHandle n_;
-    ros::Publisher pub_;
-    vector<PoseSubscriber *> subscribers_;
+private:
+  ros::NodeHandle n_;
+  ros::Publisher pub_;
+  map<string, PoseSubscriber *> subscribers_;
+  int max_num_agents_;
 
-    laser_simulator::PoseStampedNamedArray msg_;
+  string agent_prefix_;
+  string frame_id_;
+  string pose_topic_;
 
-  public:
-    PoseAggregator(ros::NodeHandle &node) : n_() {
-      int num_agents;
-      string agent_name, frame_id, pose_topic;
-      node.param("agent_prefix", agent_name, string("scarab"));
-      node.param("num_agents", num_agents, 0);
-      node.param("frame_id", frame_id, string("base_link"));
-      node.param("pose_topic", pose_topic, string("pose"));
+public:
+  PoseAggregator(ros::NodeHandle &node) : n_() {
+    int num_agents;
+    string agent_name, frame_id, pose_topic;
+    node.param("agent_prefix", agent_prefix_, string("scarab"));
+    node.param("num_agents", num_agents, 0);
+    node.param("frame_id", frame_id_, string("base_link"));
+    node.param("pose_topic", pose_topic_, string("pose"));
+    max_num_agents_ = num_agents;
 
-      msg_.poses.resize(num_agents);
+    for (unsigned int i = 0; i <= num_agents; ++i) {
+      stringstream name;
+      name << agent_prefix_ << i;
 
-      for (unsigned int i=0; i<num_agents; ++i) {
+      subscribers_[name.str()] = new PoseSubscriber(&n_, name.str(), frame_id_, pose_topic_);
+    }
+
+    pub_ = n_.advertise<laser_simulator::PoseStampedNamedArray>(
+            "pose_array", 1);
+  }
+
+  ~PoseAggregator() {
+    for (map<string, PoseSubscriber*>::iterator it = subscribers_.begin();
+         it != subscribers_.end(); ++it) {
+      delete it->second;
+    }
+  }
+
+  void Spin() {
+    laser_simulator::PoseStampedNamedArray msg;
+
+    for (map<string, PoseSubscriber*>::iterator it = subscribers_.begin();
+         it != subscribers_.end(); ++it) {
+      if (!it->second->active()) {
+        continue;
+      }
+      laser_simulator::PoseStampedNamed p;
+      if (it->second->getPose(&p)) {
+        msg.poses.push_back(p);
+      } else {
         stringstream name;
-        name << agent_name << i;
-
-        subscribers_.push_back(new PoseSubscriber(&n_, name.str(), frame_id, pose_topic));
-      }
-
-      pub_ = n_.advertise<laser_simulator::PoseStampedNamedArray>(
-              "pose_array", 1);
-    }
-
-    ~PoseAggregator() {
-      for (vector<PoseSubscriber*>::iterator it=subscribers_.begin();
-           it!=subscribers_.end(); ++it) {
-        delete *it;
+        name << agent_prefix_ << max_num_agents_;
+        if (name.str() != it->second->getName()) {
+          delete it->second;
+          subscribers_.erase(it);
+        }
       }
     }
 
-    void Spin() {
-      unsigned int j = 0;
-      for (vector<PoseSubscriber*>::iterator i=subscribers_.begin();
-           i!=subscribers_.end(); ++i, j++) {
-        msg_.poses[j] = (*i)->GetPose();
+    while (true) {
+      stringstream name;
+      name << agent_prefix_ << max_num_agents_;
+      if (subscribers_[name.str()]->getNumPublishers() > 0) {
+        stringstream new_name;
+        new_name << agent_prefix_ << ++max_num_agents_;
+        subscribers_[new_name.str()] = new PoseSubscriber(&n_, new_name.str(), frame_id_, pose_topic_);
+      } else {
+        break;
       }
-
-      pub_.publish(msg_);
     }
+
+    pub_.publish(msg);
+  }
 };
 
 
